914ec9950eaf093e21dedb355f771986b0b426ec
SumiranBhattarai <sumiran.devp.2001@gmail.com>
2023-02-02 18:38:30 +0530

    pi program

---
+++ Lab 3/pi_program.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+static long num_steps = 100000;
+double step;
+void main()
+{
+    int i;
+    double x, pi, sum = 0, end, start;
+    start = omp_get_wtime();
+    step = 1.0 / (double)num_steps;
+    for (i = 0; i < num_steps; i++)
+    {
+        x = (i + 0.5) * step;
+        sum = sum + 4.0 / (1.0 + x * x);
+    }
+    pi = step * sum;
+    printf("%f", pi);
+    end = omp_get_wtime();
+    printf("\nExecution Time: %f", end - start);
+}
